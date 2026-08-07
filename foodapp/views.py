e65fd5d75f3b087ab0e094b7b7ab1c2723eb15e5
DeFrancis-unix27 <prevailfrancis@gmail.com>
2026-08-07 07:42:22 -0400

    Implement user registration and login views with JWT authentication

--- foodapp/views.py
+++ foodapp/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth import authenticate
 from rest_framework import generics
 from .serializers import *
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 # Create your views here.
+
+
+class register(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegistrationSerializer
+
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"message": "no such user found"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "message": f"welcome {user.username} to foodbank",
+                "refresh": str(refresh),
+                "access_token": str(refresh.access_token),
+            }
+        )
+
